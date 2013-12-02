fb955adee7b29dfc2cb1402cc46a2fe65b139dae
Dmitry Bogdanov <dimabv@tut.by>
2013-12-02 23:51:02 +0100

    Featurize went to special place

--- src/loaddir.m
+++ src/loaddir.m
@@ -29,12 +29,4 @@ function [ uttrs, features, labels ] = loaddir(path)
     sprintf('loaded %d recordings', length(uttrs) / cs.n_classes)
 end
 
-function [features] = featurize(utterance)
-    cs = constants();
-    shift = cs.window_size / 2;
-    features = zeros(cs.n_features, length(utterance) / shift);
-    for i=1:size(features, 2)
-        features(:, i) = extract(utterance(shift * (i - 1) + 1:shift * i), 0, 0);
-    end;
-end
 
