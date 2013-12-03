a308eff8f842546f40bca1bde5db324d3b37e81d
Dmitry Bogdanov <dimabv@tut.by>
2013-12-03 13:56:38 +0100

    We don't need no filteriiing

--- src/featurize.m
+++ src/featurize.m
@@ -3,6 +3,6 @@ function [features] = featurize(utterance)
     shift = cs.window_size / 2;
     features = zeros(cs.n_features, length(utterance) / shift);
     for i=1:size(features, 2)
-        features(:, i) = extract(utterance(shift * (i - 1) + 1:shift * i), 1, 0);
+        features(:, i) = extract(utterance(shift * (i - 1) + 1:shift * i), 0, 0);
     end;
 end
