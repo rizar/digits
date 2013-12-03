a3b1f466c18ec2194c103395d9d9de70e69c10c8
Dmitry Bogdanov <dimabv@tut.by>
2013-12-03 15:04:45 +0100

    Add power normalization
    it was not neither beneficial nor too bad, but certainly makes the whole
    thing more reliable

--- src/featurize.m
+++ src/featurize.m
@@ -5,4 +5,7 @@ function [features] = featurize(utterance)
     for i=1:size(features, 2)
         features(:, i) = extract(utterance(shift * (i - 1) + 1:shift * i), 0, 0);
     end;
+    
+    % first feature is special
+    features(1, :) = features(1, :) / max(features(1, :));
 end
